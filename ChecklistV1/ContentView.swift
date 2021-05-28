18826f310495afb23ccd5fb257c67962cda3585b
SungYubJung <sungyup5003@gmail.com>
2021-05-28 17:32:35 +0900

    add Section & Group

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -11,12 +11,29 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             List{
-                Text("Walk the dog")
-                Text("Brush my teeth")
-                Text("Learn IOS development")
-                Text("Soccer practice")
-                Text("Eat Ice cream")
+                Section(header: Text("High priority")) {
+                    Group{
+                        Text("Walk the dog")
+                        Text("Brush my teeth")
+                        Text("Learn IOS development")
+                        Text("Make Dinner")
+                        Text("Do laundry")
+                        Text("Pay bills")
+                    }
+                    Group{
+                        Text("Finish homework")
+                        Text("Change internet provider")
+                        Text("Clean the kitchen")
+                        Text("Wash the car")
+                        Text("Wash the hair")
+                    }
+                }
+                Section(header: Text("Low priority")) {
+                    Text("Soccer practice")
+                    Text("Eat Ice cream")
+                }
             } // End of body
+            .listStyle(GroupedListStyle())
             .navigationBarTitle("Checklist")
         } // End of NavigationView
     } // End of Body
