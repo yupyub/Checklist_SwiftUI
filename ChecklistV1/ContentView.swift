5d9971aa4a110a927ac60775cff7e9b2ae7324eb
SungYubJung <sungyup5003@gmail.com>
2021-05-28 17:04:40 +0900

    Initial Commit

---
+++ ChecklistV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV1
+//
+//  Created by yup on 2021/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
