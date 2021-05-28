32beea52ee38bc3d28b7b475da4bfc2e55262a7a
SungYubJung <sungyup5003@gmail.com>
2021-05-28 18:10:52 +0900

    use Looping

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -8,22 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var checklistItems = ["Walk the dog","Brush my teeth","Learn IOS development","Make Dinner","Do laundry","Pay bills","Finish homework","Change internet provider","Clean the kitchen","Wash the car","Wash the hair","Soccer practice","Eat Ice cream"]
+    @State var checklistItems = ["Walk the dog","Brush my teeth","Learn IOS development","Soccer practice","Eat Ice cream"]
     var body: some View {
         NavigationView{
             List{
-                Text(checklistItems[0])
-                    .onTapGesture{
-                        self.checklistItems[0] = "Take the dog to the vet"
-                    }
-                Text(checklistItems[1])
-                Text(checklistItems[2])
-                Text(checklistItems[3])
-                Text(checklistItems[4])
+                ForEach(checklistItems, id: \.self){
+                    item in Text(item)
+                }
             } // End of list
             .navigationBarTitle("Checklist")
+            .onAppear(){
+                self.printChecklistContents()
+            }
         } // End of NavigationView
     } // End of Body
+    
+    // Method
+    func printChecklistContents(){
+        for item in checklistItems{
+            print(item)
+        }
+    }
+    
 } // End of ContentView
 
 struct ContentView_Previews: PreviewProvider {
