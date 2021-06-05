6b8fdb1127ed4b78bde8a09191db0f3c5437c303
SungYubJung <sungyup5003@gmail.com>
2021-06-05 23:17:35 +0900

    Change the starting scene

---
+++ ChecklistV1/EditChecklistItemView.swift
@@ -0,0 +1,20 @@
+//
+//  EditChecklistItemView.swift
+//  ChecklistV1
+//
+//  Created by yup on 2021/06/05.
+//
+
+import SwiftUI
+
+struct EditChecklistItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditChecklistItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditChecklistItemView()
+    }
+}
